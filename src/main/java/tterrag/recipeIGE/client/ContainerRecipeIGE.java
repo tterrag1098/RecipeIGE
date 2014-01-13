604cc606b6dae2f8f14a4562b9b1c61da7a3563c
Garrett S-D <tterrag1098@gmail.com>
2014-01-13 01:12:53 -0500

    Gui is now mostly functional, buttons not so much.

--- src/main/java/tterrag/recipeIGE/client/ContainerRecipeIGE.java
+++ src/main/java/tterrag/recipeIGE/client/ContainerRecipeIGE.java
@@ -12,17 +12,15 @@ import net.minecraft.item.crafting.CraftingManager;
 
 public class ContainerRecipeIGE extends Container
 {
-	public ItemStack[] inventory;
+	public ItemStack[] inventory = new ItemStack[10];
 
-	private InventoryRecipeIGE craftMatrix;
+	public InventoryRecipeIGE craftMatrix;
 	private InventoryRecipeIGE.InventoryRecipeIGEResult craftResult;
 
 	public ContainerRecipeIGE(EntityPlayer player)
 	{
-		inventory = new ItemStack[10];
-
 		craftMatrix = new InventoryRecipeIGE(this, 3, 3);
-		craftResult = craftMatrix.new InventoryRecipeIGEResult(this);
+		craftResult = craftMatrix.new InventoryRecipeIGEResult(this, craftMatrix);
 
 		InventoryPlayer inventoryplayer = player.inventory;
 
@@ -70,5 +68,53 @@ public class ContainerRecipeIGE extends Container
 	public void onCraftMatrixChanged(IInventory par1iInventory)
 	{
 		this.craftResult.setInventorySlotContents(45, CraftingManager.getInstance().findMatchingRecipe(this.craftMatrix, Minecraft.getMinecraft().theWorld));
+	}
+	
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotIndex)
+	{
+		ItemStack itemstack = null;
+		Slot slot = (Slot) this.inventorySlots.get(slotIndex);
+		if (slot != null && slot.getHasStack())
+		{
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+
+			if (slotIndex > 35)
+			{
+				if (!this.mergeItemStack(itemstack1, 27, 36, false))
+				{		System.out.println(slotIndex);
+
+					if (!this.mergeItemStack(itemstack1, 0, 27, false))
+						return null;
+				}
+				slot.onSlotChange(itemstack1, itemstack);
+			}
+			else if (slotIndex < 36)
+			{
+				if (!this.mergeItemStack(itemstack1, 36, 45, false))
+					return null;
+			}
+
+			if (itemstack1.stackSize == 0)
+			{
+				slot.putStack((ItemStack) null);
+			}
+			else
+			{
+				slot.onSlotChanged();
+			}
+			if (itemstack1.stackSize == itemstack.stackSize)
+			{
+				return null;
+			}
+			slot.onPickupFromSlot(player, itemstack1);
+			if (itemstack1.stackSize == 0)
+			{
+				slot.putStack(null);
+				return null;
+			}
+		}
+		return itemstack;
 	}
 }
--- src/main/java/tterrag/recipeIGE/inv/InventoryRecipeIGE.java
+++ src/main/java/tterrag/recipeIGE/inv/InventoryRecipeIGE.java
@@ -1,35 +1,49 @@
 package tterrag.recipeIGE.inv;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryCraftResult;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import tterrag.recipeIGE.client.ContainerRecipeIGE;
 
 public class InventoryRecipeIGE extends InventoryCrafting
 {
+	ContainerRecipeIGE container;
+
 	public InventoryRecipeIGE(Container par1Container, int size, int height)
 	{
 		super(par1Container, size, height);
+		container = (ContainerRecipeIGE) par1Container;
 	}
 
 	public class InventoryRecipeIGEResult extends InventoryCraftResult implements IInventory
 	{
-		@SuppressWarnings("unused")
 		private ContainerRecipeIGE container;
+		private InventoryRecipeIGE matrix;
 		public ItemStack[] inv;
 
-		public InventoryRecipeIGEResult(Container container)
+		public InventoryRecipeIGEResult(Container container, InventoryRecipeIGE crafter)
 		{
 			this.container = (ContainerRecipeIGE) container;
 			this.inv = new ItemStack[1];
+			this.matrix = crafter;
 		}
 
 		@Override
 		public void setInventorySlotContents(int slot, ItemStack itemstack)
 		{
-			System.out.println(slot);
 			inv[0] = itemstack;
 		}
 
@@ -38,5 +52,86 @@ public class InventoryRecipeIGE extends InventoryCrafting
 		{
 			return this.inv[0];
 		}
+
+		@SuppressWarnings({ "unchecked" })
+		@Override
+		public void onInventoryChanged()
+		{
+			super.onInventoryChanged();
+			if (matrix.isEmpty())
+			{
+				ListIterator<IRecipe> iterator = CraftingManager.getInstance().getRecipeList().listIterator();
+				ItemStack[] input = null;
+				List<ItemStack> shapelessRecipeList = null;
+				loop: while (iterator.hasNext())
+				{
+					IRecipe recipe = iterator.next();
+					if (recipe instanceof ShapedRecipes)
+						input = ((ShapedRecipes) recipe).recipeItems;
+					else if (recipe instanceof ShapelessRecipes)
+						shapelessRecipeList = ((ShapelessRecipes) recipe).recipeItems;
+					else if (recipe instanceof ShapedOreRecipe)
+					{
+						input = new ItemStack[9];
+						int idx = 0;
+						Object[] objs = ((ShapedOreRecipe)recipe).getInput();
+						for (Object o : objs)
+						{
+							if (o instanceof ItemStack)
+							{
+								input[idx] = (ItemStack) o;
+								idx++;
+							}
+							else if (o instanceof List<?> && ((List<ItemStack>)o).size() > 0)							
+							{
+								input[idx] = ((List<ItemStack>)o).get(0);
+								idx++;
+							}
+							else if (o == null)
+							{
+								input[idx] = null;
+								idx++;
+							}
+						}
+					}
+
+					if (inv[0] == null || recipe.getRecipeOutput() == null)
+						continue loop;
+
+					if (recipe.getRecipeOutput().getItem() != inv[0].getItem() || recipe.getRecipeOutput().getItemDamage() != inv[0].getItemDamage())
+						continue loop;
+					
+					if (recipe instanceof ShapedOreRecipe)
+					System.out.println(recipe.getRecipeOutput() + " : " + Arrays.deepToString(input));
+					
+					if (input != null)
+						for (int i = 0; i < 9; i++)
+						{
+							if (i < input.length)
+							{
+								container.craftMatrix.setInventorySlotContents(i, input[i]);
+							}
+						}
+					else if (shapelessRecipeList != null)
+						for (ItemStack item : shapelessRecipeList)
+						{
+							container.craftMatrix.setInventorySlotContents(shapelessRecipeList.indexOf(item), new ItemStack(item.getItem(), 1, item.getItemDamage()));
+						}
+
+					input = null;
+					shapelessRecipeList = null;
+				}
+			}
+		}
+	}
+
+	public boolean isEmpty()
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			if (container.craftMatrix.getStackInSlot(i) != null)
+				return false;
+		}
+		return true;
 	}
 }
